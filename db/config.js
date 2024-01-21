2a9adec9aca111e39556072162cdeab34cf09359
21120553 <tantran.300803@gmail.com>
2024-01-21 21:40:57 +0700

    restructre project

--- db/config.js
+++
@@ -1,6 +0,0 @@
-const config = require("../configs/connectStr");
-require("dotenv").config();
-const initOptions = {};
-const pgp = require("pg-promise")(initOptions);
-const db = pgp(config);
-module.exports = db;
